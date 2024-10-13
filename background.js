42c0b099d3eb0e0e9fa6b829445b8b3b7383d175
takasuka00 <takasuka00@gmail.com>
2024-10-13 21:57:59 +0900

    init

---
+++ background.js
@@ -0,0 +1,33 @@
+const {createFunc, changeFunc} = (() => {
+
+    /**  @type {DownloadItem|null}  */
+    let downloadItem = null;
+    /**  @param {DownloadItem} item */
+    const createFunc = (item) => {
+        downloadItem = item;
+    }
+
+    /** @type {DownloadDelta} */
+    let preDelta = null;
+    /**  @param {DownloadDelta} delta */
+    const changeFunc = (delta) => {
+        if (!downloadItem.finalUrl.endsWith(".pdf") //ダウンロード元がpdfを表示しているページのダウンロードボタンではなく
+            && delta.id === downloadItem.id //ダウンロードIDが同じで
+            && preDelta && preDelta.filename && preDelta.filename.current.endsWith(".pdf") //pdfをダウンロードしていた場合
+            && delta.state && delta.state.current === "complete" //ダウンロード完了時に
+        ) {
+            // 新しいタブでPDFを表示
+            chrome.tabs.create({url: preDelta.filename.current});
+        }
+
+        preDelta = delta;
+        if (delta.endTime) {
+            preDelta = null
+        }
+    }
+
+    return {createFunc, changeFunc};
+})()
+
+chrome.downloads.onCreated.addListener(createFunc);
+chrome.downloads.onChanged.addListener(changeFunc);
